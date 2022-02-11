515d023ed72e013d3f62af82a7796b6452370caf
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2022-02-11 16:42:20 +0100

    Simple fact

--- src/main/scala/io/github/agbaroni/tests/scala/Main.scala
+++ src/main/scala/io/github/agbaroni/tests/scala/Main.scala
@@ -1,7 +1,15 @@
 package io.github.agbaroni.tests.scala
 
 object Main {
+  def fact(n: Int): Int = {
+    if (n < 2)
+      return 1
+    else
+      return n * fact(n - 1)
+  }
+
   def main(args: Array[String]): Unit = {
     println("ciao")
+    println(fact(10))
   }
 }
